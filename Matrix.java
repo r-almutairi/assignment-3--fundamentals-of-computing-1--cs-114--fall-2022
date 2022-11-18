a02907c834615ea673ace0374768292a52009cdd
r-almutairi <113357071+r-almutairi@users.noreply.github.com>
2022-11-18 15:38:41 -0500

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,3 +1,77 @@
 public class Matrix {
+    
+    int [][] matrix ; 
+    int size;
+    
+    public Matrix(int size){
+        
+        matrix = new int[size][size];
+        this.size = size; 
+        
+         for(int i=0; i< matrix.length; i++){
+            
+            for(int j=0; j< matrix.length; j++ ){
+                
+                matrix[i][j] = 0 ; 
+            }
+            
+        }
+        
+        
+        
+    }
+    
+    
+    private void swap(int x1, int y1, int x2, int y2){
+        
+        int temp = matrix[x1][y1];
+        
+        matrix[x1][y1] = matrix[x2][y2] ;
+        matrix[x2][y2] = temp;
+        
+    }
+    
+    
+    public void printMatrix(){
+        
+        
+        System.out.println();
+        for(int i=0; i< matrix.length; i++){
+            
+            for(int j=0; j< matrix.length; j++ ){
+                
+                System.out.print(matrix[i][j]+"   ");
+            }
+            System.out.println();
+        }
+    }
+    
+    
+    public void populateMatrix(){
+        
+        
+        int start=1;
+        for(int i=0; i< matrix.length; i++){
+            
+            for(int j=0; j< matrix.length; j++ ){
+                
+                matrix[i][j] = start;
+                start++;
+            }
+            
+        }
+        
+        
+    }
+    
+    
+    public void flipMatrix(){
+        
+        
+    }
+    
+    
+    
+    
 
 }
